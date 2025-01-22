eae6d853a1be00bb06fcd0162ea3d3938721a7f2
Eduardo Peral <duduperal@gmail.com>
2025-01-22 15:11:04 -0300

    Projeto completo

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
+let qtdJogosAlugados = 0;
+function exibirJogosAlugados(){
+    console.log(qtdJogosAlugados);
+}
 function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nome = gameClicado.querySelector('.dashboard__item__name');
     if(imagem.classList.contains('dashboard__item__img--rented')){
-        imagem.classList.remove("dashboard__item__img--rented");
+        if(confirm(`Porfavor confirme que você quer devolver o jogo ${nome.innerHTML}`)){
+            imagem.classList.remove("dashboard__item__img--rented");
+            botao.classList.remove("dashboard__item__button--return");
+            botao.innerHTML = "Alugar";
+        }
     }else{
         imagem.classList.add("dashboard__item__img--rented");
-    }
-    if(botao.classList.contains("dashboard__item__button--return")){
-        botao.classList.remove("dashboard__item__button--return");
-        botao.innerHTML = "Alugar";
-    }else{
         botao.classList.add("dashboard__item__button--return");
         botao.innerHTML = "Devolver";
+        qtdJogosAlugados++;
     }
+    exibirJogosAlugados();
 }
-
